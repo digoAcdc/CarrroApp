d4437f30c28660e52f75d74210d7bd37147acfde
digoAcdc <barbosa_silveira@hotmail.com>
2016-02-29 19:36:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rm40217/carrroapp/adapter/TabsAdapter.java
@@ -0,0 +1,43 @@
+package com.example.rm40217.carrroapp.adapter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.rm40217.carrroapp.R;
+import com.example.rm40217.carrroapp.fragment.FragmentCarros;
+
+/**
+ * Created by rm40217 on 19/11/2015.
+ */
+public class TabsAdapter extends FragmentStatePagerAdapter {
+
+    public TabsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        FragmentCarros fragment = new FragmentCarros();
+        Bundle args = new Bundle();
+        switch (position){
+            case 0:
+                args.putInt("tipo", R.raw.carros_classicos);
+                break;
+            case 1:
+                args.putInt("tipo", R.raw.carros_esportivos);
+                break;
+            case 2:
+                args.putInt("tipo", R.raw.carros_luxuosos);
+                break;
+        }
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/rm40217/carrroapp/fragment/FragmentCarros.java
@@ -0,0 +1,60 @@
+package com.example.rm40217.carrroapp.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rm40217.carrroapp.R;
+import com.example.rm40217.carrroapp.adapter.CarroAdapter;
+import com.example.rm40217.carrroapp.model.Carro;
+import com.example.rm40217.carrroapp.service.CarroService;
+
+import java.util.List;
+
+public class FragmentCarros extends Fragment {
+
+    private List<Carro> carros;
+    private RecyclerView rvCarros;
+    private LinearLayoutManager layoutManager;
+    private int tipo;
+
+    public FragmentCarros() {
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        if(getArguments() != null) {
+            this.tipo = getArguments().getInt("tipo");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_carros, container, false);
+        rvCarros = (RecyclerView) view.findViewById(R.id.rvCarros);
+        layoutManager = new LinearLayoutManager(getContext());
+        rvCarros.setLayoutManager(layoutManager);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle bundle) {
+        super.onActivityCreated(bundle);
+        taskCarros();
+    }
+
+    private void taskCarros() {
+        CarroService servico = new CarroService(getContext());
+        carros = servico.getCarros(servico.loadJSONFromRaw(tipo));
+        rvCarros.setAdapter(new CarroAdapter(getContext(), carros));
+    }
+
+
+}
